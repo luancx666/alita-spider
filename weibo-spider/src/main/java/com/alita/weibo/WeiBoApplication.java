cec4b8f7648f5fdc2f2d80dcbd4e189805eb63c2
栾成翔 <luancx@mti-sh.cn>
2022-06-28 14:48:26 +0800

    1

---
+++ weibo-spider/src/main/java/com/alita/weibo/WeiBoApplication.java
@@ -0,0 +1,16 @@
+package com.alita.weibo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description: 微博爬虫
+ * @author: Luancx
+ * @date: 2022/6/28
+ */
+@SpringBootApplication
+public class WeiBoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeiBoApplication.class, args);
+    }
+}
